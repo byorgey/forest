a6ef743e2cd473486daa2e807c8688c7d78c1bcb
Brent Yorgey <byorgey@gmail.com>
2026-07-10 08:38:52 -0500

    weeknotes

--- build-forest.sh
+++ build-forest.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-while sleep 0.1; do /bin/ls trees-raw/**/*.tree | entr -d -s './build.sh; forester build forest.toml'; done
+#!/bin/zsh
+while sleep 0.1; do /bin/ls trees-raw/**/*.tree | /bin/entr -d -s './build.sh; /home/brent/.opam/default/bin/forester build forest-out.toml'; done
